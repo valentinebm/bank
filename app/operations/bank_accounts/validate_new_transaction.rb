2badd5a63d31c4ef098b92db68e1d10fcdf9244c
Valentine BM <vbmueller@bmgww.com>
2018-07-09 15:04:33 +0800

    last commit

---
+++ app/operations/bank_accounts/validate_new_transaction.rb
@@ -0,0 +1,4 @@
+module BankAccounts
+  class ValidateNewTransaction
+  end
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      post "bank_accounts/new_transaction", to: "bank_accounts#new_transaction"
     end
   end
-  
+
 end
